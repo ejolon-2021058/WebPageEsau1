bf92ed24c5ddc500fb3c4abadd5940e4739f3a1e
Esau Jolon <ejolon-2021058@kinal.edu.gt>
2023-01-20 08:58:40 -0600

    primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+function saludar(){
+    let numA = document.getElementById("valorA").value;
+    let numB = document.getElementById("valorB").value;
+
+    let resultado = parseInt(numA) + parseInt(numB);
+
+    console.log(resultado);
+
+
+    // console.log('Hola mundo');
+}
+
